2dfcb4210b4916062f642b4e61ae6ab70027e761
Muhammadreza Haghiri <haghiri75@gmail.com>
2022-05-15 15:37:44 +0430

    Now let's connect the damn thing to the database

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/receive', methods=["POST"])
-def receive():
-    pass
-
+@app.route('/getMessage', methods=["POST"])
+def getMessage():
+    if request.method == "POST":
+        input_data = request.data
+        json_data = json.loads(input_data)
+        print(json_data)
+    return {'msg' : 1}
+    
 if __name__ == '__main__':
     app.run(debug=True)
