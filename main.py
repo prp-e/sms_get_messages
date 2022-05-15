4de79e1e9a28aa64ceb2114702b4464a1aea25c6
Muhammadreza Haghiri <haghiri75@gmail.com>
2022-05-15 16:10:42 +0430

    Now it works fine

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, g
 import json
 import os
 import sqlite3
 from uuid import uuid4
 
-DATABASE = os.path.join('.', 'db.sqlite3')
 app = Flask(__name__)
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
 
 @app.route('/getMessage', methods=["POST"])
 def getMessage():
@@ -25,9 +12,8 @@ def getMessage():
         input_data = request.data
         json_data = json.loads(input_data)
         try:
-            cur = get_db().cursor()
-            cur.execute('INSERT INTO messages (from_number, to_number, message) VALUES (?, ?, ?)', (json_data['from'], json_data['to'], json_data['message']))
-            cur.close()
+            f = open('result.txt', 'a')
+            f.write(f'{json_data["from"]}, {json_data["to"]}, {json_data["message"]} \n')
             return {'msg' : 'success'}, 200
         except Exception as e:
             print(e)
